528436df922490aa6f592f8cbe3f0e23bab6cb37
pygamer0 <maanasyt@gmail.com>
2021-04-25 13:48:15 +0530

    Done

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 import pyglet
 from freegames import vector
+from math import sin, cos
 
-class entity:
-    def __init__(self, x, y, m):
-        self.pos = vector(x, y)
-        self.vel = vector(0, 0)
-        self.acc = vector(0, 0)
-        self.mass = m
+# Window
+win = pyglet.window.Window(500, 500, "Pendulum")
 
-    def apply_force(self, f):
-        self.acc += f / self.mass
+# Bob
+bob = vector(250, 250)
 
-    def update(self):
-        self.vel += self.acc
-        self.pos += self.vel
-        self.acc *= 0
+# Arm
+origin = vector(250, 350)
+l = 100
+angle = 45
+avel = 0
+acc = 0
 
-    def draw(self, r, c):
-        shape = pyglet.shapes.Circle(self.pos.x, self.pos.y, r)
-        shape.color = c
-        shape.draw()
-
-win = pyglet.window.Window(width=500, height=500)
-e = entity(250, 250, 1)
-
+# Update
 def update(dt):
-    e.update()
+    global angle, avel, acc
 
-    # Gravity
-    g = vector(0, -0.1)
-    e.apply_force(g)
-
-@win.event
-def on_draw():
     win.clear()
-    e.draw(25, (243, 122, 20))
 
-pyglet.clock.schedule_interval(update, 1/120)
+    bob.x = origin.x + l * sin(angle)
+    bob.y = origin.y + -l * cos(angle)
+
+    pyglet.shapes.Line(origin.x, origin.y, bob.x, bob.y).draw()
+    pyglet.shapes.Circle(bob.x, bob.y, 15, color=(234,23,12)).draw()
+
+    acc = -0.01 * sin(angle)
+
+    angle += avel
+    avel += acc
+
+    avel *= 0.99
+
+pyglet.clock.schedule_interval(update, 1/60)
 pyglet.app.run()
